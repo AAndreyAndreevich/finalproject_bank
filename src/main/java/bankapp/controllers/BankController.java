d5234f40e3188d02f1f472cb628e7e814b2ef0f1
AAndreyAndreevich <andrey.zolotarev7610@gmail.com>
2024-05-19 16:50:09 +0300

    part1

--- src/main/java/bankapp/controllers/BankController.java
+++ src/main/java/bankapp/controllers/BankController.java
@@ -1,9 +1,7 @@
 package bankapp.controllers;
 
 import bankapp.methods.BankMethods;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
