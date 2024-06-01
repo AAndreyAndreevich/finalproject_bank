d9f5ef48ae93632389a7b2b7389b5aaf4dbec987
AAndreyAndreevich <andrey.zolotarev7610@gmail.com>
2024-06-01 21:37:54 +0300

    part3

--- bankdb.sql
+++ bankdb.sql
@@ -21,6 +21,43 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
+-- Name: bank_operation; Type: TABLE; Schema: public; Owner: user
+--
+
+CREATE TABLE public.bank_operation (
+    id integer NOT NULL,
+    cur_operation character varying(255),
+    date_operation date,
+    id_person integer,
+    money double precision
+);
+
+
+ALTER TABLE public.bank_operation OWNER TO "user";
+
+--
+-- Name: bank_operation_id_seq; Type: SEQUENCE; Schema: public; Owner: user
+--
+
+CREATE SEQUENCE public.bank_operation_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.bank_operation_id_seq OWNER TO "user";
+
+--
+-- Name: bank_operation_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: user
+--
+
+ALTER SEQUENCE public.bank_operation_id_seq OWNED BY public.bank_operation.id;
+
+
+--
 -- Name: bank_user; Type: TABLE; Schema: public; Owner: user
 --
 
@@ -47,19 +84,56 @@ ALTER TABLE public.bank_user ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
 
 
 --
+-- Name: bank_operation id; Type: DEFAULT; Schema: public; Owner: user
+--
+
+ALTER TABLE ONLY public.bank_operation ALTER COLUMN id SET DEFAULT nextval('public.bank_operation_id_seq'::regclass);
+
+
+--
+-- Data for Name: bank_operation; Type: TABLE DATA; Schema: public; Owner: user
+--
+
+COPY public.bank_operation (id, cur_operation, date_operation, id_person, money) FROM stdin;
+3	снятие	2024-06-01	1	20
+1	пополнение	2024-05-01	1	10
+2	пополнение	2024-05-03	3	20
+4	перевод на id : 3	2024-06-01	1	30
+5	перевод на id : 1	2024-06-01	3	80
+\.
+
+
+--
 -- Data for Name: bank_user; Type: TABLE DATA; Schema: public; Owner: user
 --
 
 COPY public.bank_user (id, balance) FROM stdin;
-1	6
+2	20
+3	30
+1	80
 \.
+
+
+--
+-- Name: bank_operation_id_seq; Type: SEQUENCE SET; Schema: public; Owner: user
+--
+
+SELECT pg_catalog.setval('public.bank_operation_id_seq', 5, true);
 
 
 --
 -- Name: bank_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: user
 --
 
-SELECT pg_catalog.setval('public.bank_user_id_seq', 1, true);
+SELECT pg_catalog.setval('public.bank_user_id_seq', 3, true);
+
+
+--
+-- Name: bank_operation bank_operation_pkey; Type: CONSTRAINT; Schema: public; Owner: user
+--
+
+ALTER TABLE ONLY public.bank_operation
+    ADD CONSTRAINT bank_operation_pkey PRIMARY KEY (id);
 
 
 --
